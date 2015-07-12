4da13dded966c057baf7d11fa84d290d7a1969ea
Austin Valle <austinvalle@gmail.com>
2015-07-12 01:06:41 -0400

    Added toJSON function with filtering

--- projecteval/api/models/db.py
+++ projecteval/api/models/db.py
@@ -1,11 +1,33 @@
 from projecteval import db
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Table, Column, Integer, ForeignKey
+from projecteval.controllers.helpers import parse_fields
 
 class Base(db.Model):
 	__abstract__ = True
 
  	id = db.Column(db.Integer, primary_key=True)
+
+ 	def jsonify_object(self):
+ 		return {}
+
+ 	def toJSON(self, fields=None):
+		if fields is None or fields == '':
+			return self.jsonify_object()
+
+		parsed = parse_fields(fields)
+		json = {}
+		for field, subfields in parsed.items():
+			v = getattr(self,field)
+			if v is not None:
+				if subfields == '':
+					json[field] = v
+				elif isinstance(v, list):
+					json[field] = [item.toJSON(subfields) for item in v]
+				else:
+					json[field] = v.toJSON(subfields)
+
+		return json
 
 class Gameplatform(Base):
 	__tablename__ = "gameplatform"
@@ -13,13 +35,12 @@ class Gameplatform(Base):
 	game_id = db.Column(db.Integer, ForeignKey('game.id'))
 	platform_id = db.Column(db.Integer, ForeignKey('platform.id'))
 	platform = relationship('Platform', backref='game_assocs')
-    
-	def __init__(self, game_id, platform_id):
-		self.game_id = game_id
-		self.platform_id = platform_id
 
 	def __repr__(self):
 		return "<Gameplatform('%s')" % self.id
+
+	def toJSON(self, fields=None):
+		return self.platform.toJSON(fields)
 
 class Game(Base):
 	__tablename__ = 'game'
@@ -39,17 +60,32 @@ class Game(Base):
 	platforms = relationship('Gameplatform', backref='games')
 	esrb = relationship('ESRB', backref='games')
 
-	def __init__(self, title, desc, developer, publisher, trailer_url, added_by, last_modified_by):
-		self.title = title
-		self.desc = desc
-		self.developer = developer
-		self.publisher = publisher
-		self.trailer_url = trailer_url
-		self.added_by = added_by
-		self.last_modified_by = last_modified_by
-
 	def __repr__(self):
 		return '<Game %s>' % (self.title)
+
+	def jsonify_object(self):
+		platforms = []
+		for gameplatform in self.platforms:
+			platforms.append(gameplatform.toJSON())
+
+		return {
+    		'id': self.id,
+    		'title':self.title,
+    		'release_date':self.release_date,
+    		'desc':self.desc,
+    		'developer':self.developer,
+    		'publisher':self.publisher,
+    		'trailer_url':self.trailer_url,
+    		'esrb_id':self.esrb_id,
+    		'genre_id':self.genre_id,
+    		'added_by':self.added_by,
+    		'date_added':self.date_added,
+    		'last_modified_by':self.last_modified_by,
+    		'last_modified':self.last_modified,
+    		'platforms': platforms,
+    		'esrb_url': self.esrb.image_url
+    	}
+
 
 class Platform(Base):
 	__tablename__ = 'platform'
@@ -68,18 +104,26 @@ class Platform(Base):
 	last_modified_by = db.Column(db.String(128), nullable=False)
 	last_modified = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-	def __init__(self, name, desc, developer, manufacturer, cpu, memory, graphics, storage):
-		self.name = name
-		self.desc = desc
-		self.developer = developer
-		self.manufacturer = manufacturer
-		self.cpu = cpu
-		self.memory = memory
-		self.graphics = graphics
-		self.storage = storage
-
 	def __repr__(self):
 		return '<Platform %s>' % (self.name)
+
+	def jsonify_object(self):
+		return {
+        	'id':self.id,
+        	'name':self.name,
+        	'release_date':self.release_date,
+        	'desc':self.desc,
+        	'developer':self.developer,
+        	'manufacturer':self.manufacturer,
+        	'cpu':self.cpu,
+        	'memory':self.memory,
+        	'graphics':self.graphics,
+        	'storage':self.storage,
+        	'added_by':self.added_by,
+        	'date_added':self.date_added,
+        	'last_modified_by':self.last_modified_by,
+        	'last_modified':self.last_modified
+    	}
 
 class ESRB(Base):
 	__tablename__ = 'esrb'	
@@ -87,11 +131,6 @@ class ESRB(Base):
 	short_desc = db.Column(db.String(128))
 	full_desc = db.Column(db.String(600))
 	image_url = db.Column(db.String(128))
-
-	def __init__(self, short_desc, full_desc, image_url):
-		self.short_desc = short_desc
-		self.full_desc = full_desc
-		self.image_url = image_url
 
 	def __repr__(self):
 		return '<ESRB %s>' % (self.short_desc)		
@@ -106,12 +145,5 @@ class User(Base):
     last_login = db.Column(db.DateTime, default=db.func.current_timestamp())
     session_id = db.Column(db.String(128))
 
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
-
     def __repr__(self):
         return '<User %r>' % (self.username)
-
-
