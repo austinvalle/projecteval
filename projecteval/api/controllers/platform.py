c4b13a40feae027c8efbac05662db34520b08e31
Austin Valle <austinvalle@gmail.com>
2015-07-12 01:05:32 -0400

    Moved jsonify to models

--- projecteval/api/controllers/platform.py
+++ projecteval/api/controllers/platform.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, abort
+from flask import Blueprint, jsonify, abort, request
 
 from projecteval.api.models.db import Platform
 
@@ -6,28 +6,14 @@ platformapi = Blueprint('platformapi', __name__)
 
 @platformapi.route('/api/platforms/', methods=['GET'])
 def all_platforms():
+	fields = request.args.get('fields')
 	platforms = Platform.query.all()
 
 	json_result = []
 
 	for platform in platforms:
-		p = {
-			'id':platform.id,
-			'name':platform.name,
-			'release_date':platform.release_date,
-			'desc':platform.desc,
-			'developer':platform.developer,
-			'manufacturer':platform.manufacturer,
-			'cpu':platform.cpu,
-			'memory':platform.memory,
-			'graphics':platform.graphics,
-			'storage':platform.storage,
-			'added_by':platform.added_by,
-			'date_added':platform.date_added,
-			'last_modified_by':platform.last_modified_by,
-			'last_modified':platform.last_modified
-		}
-		json_result.append(p)
+		json_result.append(platform.toJSON(fields))
+
 	return jsonify(platforms=json_result)
 
 @platformapi.route('/api/platforms/<int:id>', methods=['GET'])
@@ -37,21 +23,4 @@ def platform_info(id):
 	if(platform == None):
 		abort(404)
 
-	json_result = {
-		'id':platform.id,
-		'name':platform.name,
-		'release_date':platform.release_date,
-		'desc':platform.desc,
-		'developer':platform.developer,
-		'manufacturer':platform.manufacturer,
-		'cpu':platform.cpu,
-		'memory':platform.memory,
-		'graphics':platform.graphics,
-		'storage':platform.storage,
-		'added_by':platform.added_by,
-		'date_added':platform.date_added,
-		'last_modified_by':platform.last_modified_by,
-		'last_modified':platform.last_modified
-	}
-
-	return jsonify(platform=json_result)
+	return jsonify(platform=platform.toJSON())
