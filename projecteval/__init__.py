5ab0b257199713138c764fc7d7de225b937584b6
Austin Valle <austinvalle@gmail.com>
2015-07-12 01:45:41 -0400

    removed unused APIs

--- projecteval/__init__.py
+++ projecteval/__init__.py
@@ -11,9 +11,7 @@ db = SQLAlchemy(projecteval)
 
 from projecteval.api.controllers.game import gameapi as api_game
 from projecteval.api.controllers.platform import platformapi as api_platform
-from projecteval.api.controllers.esrb import esrbapi as api_esrb
 from projecteval.api.controllers.user import userapi as api_user
-from projecteval.api.controllers.error import errorapi as api_error
 
 from projecteval.controllers.games import games as games_module
 from projecteval.controllers.platforms import platforms as platform_module
@@ -22,9 +20,7 @@ from projecteval.controllers.home import home as home_module
 
 projecteval.register_blueprint(api_game)
 projecteval.register_blueprint(api_platform)
-projecteval.register_blueprint(api_esrb)
 projecteval.register_blueprint(api_user)
-projecteval.register_blueprint(api_error)
 
 projecteval.register_blueprint(games_module)
 projecteval.register_blueprint(platform_module)
--- projecteval/api/controllers/error.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify, make_response
-
-errorapi = Blueprint('errorapi', __name__)
-
-@errorapi.errorhandler(404)
-def not_found(error):
-	return make_response(jsonify({'error': 'Resource not found'}), 404)
